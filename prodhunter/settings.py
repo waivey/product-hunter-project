f7213285e8ee803e8907b4e9f178782a742015ca
waivey <waivey@gmail.com>
2019-07-20 19:15:03 +0100

    polishing

--- prodhunter/settings.py
+++ prodhunter/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'prodhunterdb',
         'USER':'postgres',
-        'PASSWORD': 'wookat05',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
